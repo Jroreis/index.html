1bb320ef92073b38885a5525a52d82725de5a288
Jefferson Reis Oliveira <jroreis4582@gmail.com>
2026-06-20 15:25:30 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,28 @@
-const CACHE_NAME = 'voltcalc-v1';
-const assets = ['/', '/index.html'];
+// Mude este número sempre que fizer um deploy importante (v1, v2, v3...)
+const CACHE_NAME = 'voltcalc-v1'; 
+const ASSETS = ['/', '/index.html', '/21310.png', '/manifest.json'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME ? caches.delete(k) : null)))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, res.clone());
+          return res;
+        });
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
